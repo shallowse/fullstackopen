46826cf897d76a3621c4d0731a9265d8e33f6cfa
shallowse <noemail>
2020-10-30 23:23:35 +0000

    osa2 2.11 with IP address not localhost

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
+  /*
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
-
+  */
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterByName, setFilterByName] = useState('');
+
+  useEffect(() => {
+    // TBD: add localhost to URL
+    axios.get('http://192.168.0.23:3001/persons')
+      .then(response => {
+        console.log('promise fullfilled', response);
+        setPersons(response.data);
+      });
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
