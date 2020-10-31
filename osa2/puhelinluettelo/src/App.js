c7846ef58b6b33753b6a2add1865020b0b46b19e
shallowse <noemail>
2020-10-31 11:28:04 +0000

    osa2: 2.15-2.18

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import personService from './services/persons';
 
 const App = () => {
   /*
@@ -18,14 +18,24 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('');
   const [filterByName, setFilterByName] = useState('');
 
+  // Get all the persons from the server to a local list
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         //console.log('promise fullfilled', response);
-        setPersons(response.data);
+        setPersons(response);
+      })
+      .catch(error => {
+        alert(`Error while fetching (GET) person data from the server: ${error}`);
+        setPersons([]);
       });
   }, []);
 
+  // Submit the data to the server
+  // Either
+  //  (1) add a new person
+  //  (2) update existing person's phone number
   const handleSubmit = (event) => {
     event.preventDefault();
     //console.log('name', newName === '');
@@ -33,20 +43,69 @@ const App = () => {
       return;
     }
 
+
+    // Update an existing person
     if (persons.find(x => x.name === newName) !== undefined) {
-      alert(`${newName} is already added to phonebook`);
-      return;
+      const result = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`);
+      if (result !== true) {
+        return;
+      }
+
+      const person = persons.find(x => x.name === newName);
+      const updatePerson = {
+        ...person,
+        number: newNumber,
+      }
+
+      personService
+        .updatePerson(updatePerson)
+        .then(response => {
+          //console.log(response);
+          // Update the local copy of persons
+          const update = persons.map(person => {
+            return person.id === response.id ? { ...person, number: response.number } : person;
+          });
+          setPersons(update);
+        })
+        .catch(error => alert(`Error while updating (PUT) new person data ${updatePerson.name} to the server: ${error}`));
+    }
+    // Add a new person
+    else {
+      const newPerson = {
+        name: newName,
+        number: newNumber,
+      };
+      personService
+        .postPerson(newPerson)
+        .then(response => {
+          console.log(response);
+          setPersons(persons.concat(response));
+        })
+        .catch(error => alert(`Error while posting (POST) new person data to the server: ${error}`));
     }
 
-    const newPerson = {
-      name: newName,
-      number: newNumber,
-    };
-
-    setPersons(persons.concat(newPerson));
     setNewName('');
     setNewNumber('');
   };
+
+  // Delete a person from the server
+  const handlePersonDelete = (person) => {
+    console.log('Delete :: person', person.name);
+    const result = window.confirm(`Delete ${person.name}`);
+    if (result !== true) {
+      return;
+    }
+
+    personService
+      .deletePerson(person.id)
+      .then(response => {
+        const updatedPersonList = persons.filter(x => x.id !== person.id);
+        //console.log(updatedPersonList);
+        setPersons(updatedPersonList);
+      }
+      )
+      .catch(error => alert(`Error while deleting (DELETE) person (${person.name}) data from the server: ${error}`))
+  }
 
   const handleNameChange = (event) => {
     //console.log(event.target.value);
@@ -88,7 +147,10 @@ const App = () => {
 
       <h3>Numbers</h3>
 
-      <Persons personsToShow={personsToShow} />
+      <Persons
+        personsToShow={personsToShow}
+        handlePersonDelete={handlePersonDelete}
+      />
     </div>
   );
 };
--- osa2/puhelinluettelo/src/components/Persons.js
+++ osa2/puhelinluettelo/src/components/Persons.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const Persons = ({
   personsToShow = [],
+  handlePersonDelete = f => f,
 }) => {
   return (
     <>
       {
-        personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+        personsToShow.map(person =>
+          <p key={person.name}>
+            {person.name} {person.number} {' '}
+            <button onClick={() => handlePersonDelete(person)}>delete</button>
+          </p>)
       }
     </>
   );
