c76f0785b69b2057fff109fb870a9ded2c146aac
shallowse <noemail>
2020-11-04 16:28:25 +0000

    osa5: 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false);
+
+  const blogStyle = {
+    paddingTop: '10px',
+    paddingLeft: '2px',
+    border: 'solid',
+    borderWidth: '1px',
+    marginBottom: '5px',
+  };
+
+  if (showAll) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}{' ' }
+        <button onClick={() => setShowAll(!showAll)}>hide</button><br />
+        {blog.url}<br />
+        likes{' '}{blog.likes}{' '}<button>like</button><br />
+        {blog.user.name}{' '}
+      </div>
+    );
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}{' '}
+        <button onClick={() => setShowAll(!showAll)}>view</button>
+      </div>
+    );
+  }
+};
 
 export default Blog;
