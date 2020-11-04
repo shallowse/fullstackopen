b701d82fe2b63cdfd3e010e4178d9927deadcb28
shallowse <noemail>
2020-11-04 16:09:25 +0000

    osa5: refactored login form

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Blog from './components/Blog';
+import BlogList from './components/BlogList';
 import Notification from './components/Notification';
 import LoginForm from './components/LoginForm';
 import NewBlogForm from './components/NewBlogForm';
 import blogService from './services/blogs';
 import loginService from './services/login';
 import Togglable from './components/Togglable';
-
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
 
@@ -36,8 +33,7 @@ const App = () => {
     }, 5000);
   };
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
+  const handleLogin = async (username, password) => {
     try {
       const user = await loginService.login({ username, password });
       window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user));
@@ -47,9 +43,6 @@ const App = () => {
       notifyUser('wrong username or password');
       console.log(error);
     }
-
-    setUsername('');
-    setPassword('');
   };
 
   const handleLogout = () => {
@@ -77,13 +70,7 @@ const App = () => {
 
       <div>
         {user === null ?
-          <LoginForm
-            username={username}
-            password={password}
-            handleSetUsername={({ target }) => setUsername(target.value)}
-            handleSetPassword={({ target }) => setPassword(target.value)}
-            handleSubmit={handleLogin}
-          />
+          <LoginForm handleSubmit={handleLogin} />
           :
           <div>
             {user.name} logged in{' '}
@@ -109,11 +96,7 @@ const App = () => {
 
       {
         user !== null &&
-        <div>
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
-          )}
-        </div>
+        <BlogList blogs={blogs} />
       }
     </>
   );
--- osa5/bloglist-frontend/src/components/LoginForm.js
+++ osa5/bloglist-frontend/src/components/LoginForm.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginForm = ({
-  username = '',
-  password = '',
-  handleSetUsername = f => f,
-  handleSetPassword = f => f,
   handleSubmit = f => f,
 }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    handleSubmit(username, password);
+    setUsername('');
+    setPassword('');
+  };
+
   return (
     <div>
       <h2>Log in to application</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleLogin}>
         <div>
           username:{' '}
           <input
             type='text'
             value={username}
             name='username'
-            onChange={handleSetUsername}
+            onChange={({target}) => setUsername(target.value)}
           />
         </div>
         <div>
@@ -26,7 +32,7 @@ const LoginForm = ({
             type='password'
             value={password}
             name='password'
-            onChange={handleSetPassword}
+            onChange={({target}) => setPassword(target.value)}
           />
         </div>
         <button type='submit'>login</button>
