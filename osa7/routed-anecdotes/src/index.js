6c43ed7dc78db8d5bf76807031cf8dc1337568cf
shallowse <noemail>
2020-11-12 18:23:18 +0000

    osa7: add React.StricMode

--- osa7/routed-anecdotes/src/index.js
+++ osa7/routed-anecdotes/src/index.js
@@ -5,8 +5,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 );
