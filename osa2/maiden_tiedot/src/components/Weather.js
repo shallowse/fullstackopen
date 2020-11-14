2ffbc041a2789b81bd4b14e2e46699f74f3ffb0d
shallowse <noemail>
2020-11-14 12:18:51 +0000

    osa2: maiden_tiedot, fixed a bug related to unmounted component

--- osa2/maiden_tiedot/src/components/Weather.js
+++ osa2/maiden_tiedot/src/components/Weather.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 const Weather = ({ countryLatLngArr }) => {
   const [weatherData, setWeatherData] = useState('');
-  //console.log(countryLatLngArr);
 
   useEffect(() => {
     // Found this kind of weather API that does not require an API key
@@ -14,23 +13,43 @@ const Weather = ({ countryLatLngArr }) => {
     // Example country: Finland
     // http://www.7timer.info/bin/api.pl?lon=26&lat=64&product=civillight&output=json
 
-
     const lat = countryLatLngArr[0];
     const lng = countryLatLngArr[1];
     //console.log(lat, lng);
     const url = `http://www.7timer.info/bin/api.pl?lon=${lng}&lat=${lat}&product=civillight&output=json`;
     console.log(`Countries :: useEffect :: calling ${url}`);
 
+    const CancelToken = axios.CancelToken;
+    const source = CancelToken.source();
+
     axios
-      .get(url)
+      .get(url, { cancelToken: source.token })
       .then(response => {
         console.log(response.data.dataseries[0]);
         setWeatherData(response.data.dataseries[0]);
       })
       .catch(error => console.log('ERROR', error));
 
+    // The below return function is used to prevent the following error in case the Weather component
+    // has been unmounted before the weather data has arrived.
+    //
+    // Error: encountered when using <React.StrinctMode>
+    //  "Can't perform a React state update on an unmounted component.
+    //  This is a no-op, but it indicates a memory leak in your application.
+    //  To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function."
+    //
+    // This case happens when the component is still fetching the weather data and the user clears out the
+    // filter field in the 'find countries' input.
+    //
+    // The React doumentation has the following to say about cleanup in useEffect
+    // https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+    //
+    // The axios request is cancelled when the component is unmounted by
+    // following the instructions from axios pages: https://github.com/axios/axios#cancellation
+    return () => {
+      source.cancel('Component was maybe unmounted, weather data fetch operation cancelled');
+    }
   }, [countryLatLngArr]);
-
 
   if (weatherData === '') {
     return <p>Fetching weather data from <code>http://www.7timer.info</code> ...</p>
