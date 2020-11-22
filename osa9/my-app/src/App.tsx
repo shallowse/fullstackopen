03c5281a822c248f3127923a603f3973a2ea4a71
shallowse <noemail>
2020-11-22 18:55:39 +0000

    osa9: 9.15

--- osa9/my-app/src/App.tsx
+++ osa9/my-app/src/App.tsx
@@ -1,24 +1,96 @@
 import React from 'react';
-import { CourseName, Courses } from './types';
 
-const Header: React.FC<CourseName> = (props) => {
+export interface CourseName {
+  name: string;
+}
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CourseDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartBase, CourseDescription {
+  name: 'Fundamentals';
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: 'Using props to pass data';
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartBase, CourseDescription {
+  name: 'Deeper type usage';
+  exerciseSubmissionLink: string;
+}
+
+interface CourseAddYourOwn extends CoursePartBase, CourseDescription {
+  name: 'Add your own';
+  extra: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CourseAddYourOwn;
+
+interface Courses {
+  courseParts: CoursePart[];
+}
+
+interface Course {
+  course: CoursePart;
+}
+
+const Header: React.FC<CourseName> = ({ name }) => {
   return (
-    <h1>{props.name}</h1>
+    <h1>{name}</h1>
   );
 };
 
+const Part: React.FC<Course> = (props) => {
+  //console.log(props);
+
+  const course: CoursePart = props.course;
+  let retElem: JSX.Element;
+
+  switch (course.name) {
+    case 'Fundamentals':
+      retElem = (<p><em>{course.name}:</em>{' '}
+      (descpription: {course.description}){' '}
+      (exerciseCount: {course.exerciseCount})</p>);
+      break;
+    case 'Using props to pass data':
+      retElem = (<p><em>{course.name}:</em>{' '}
+      (exerciseCount: {course.exerciseCount}){' '}
+      (groupProjectCount: {course.groupProjectCount})</p>);
+      break;
+    case 'Deeper type usage':
+      retElem = (<p><em>{course.name}:</em>{' '}
+        (description: {course.description}){' '}
+        (exerciseCount: {course.exerciseCount}){' '}
+        (exerciseSumbmissionlink: {course.exerciseSubmissionLink})</p>);
+      break;
+    case 'Add your own':
+      retElem = (<p><em>{course.name}:</em>{' '}
+      (descpription: {course.description}){' '}
+      (exerciseCount: {course.exerciseCount})
+      (extra: {course.extra})</p>);
+      break;
+    default:
+      retElem = <p>Unknown name...</p>;
+  }
+  return retElem;
+};
+
 const Content: React.FC<Courses> = ({ courseParts }) => {
-  // console.log(courseParts);
   return (
     <div>
-      {
-        courseParts.map(course =>
-          <p key={course.name}>{course.name}{' '}{course.exerciseCount}</p>)
-      }
+      {courseParts.map(course => <Part key={course.name} course={course} />)}
     </div>
   );
+  return <div>Hello</div>;
 }
-
 const Total: React.FC<Courses> = ({ courseParts }) => {
   return (
     <p>
@@ -30,19 +102,29 @@ const Total: React.FC<Courses> = ({ courseParts }) => {
 
 const App: React.FC = () => {
   const courseName = 'Half Stack application development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: 'This is an awesome course part'
     },
     {
       name: 'Using props to pass data',
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: 'Deeper type usage',
-      exerciseCount: 14
-    }
+      exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev'
+    },
+    {
+      name: 'Add your own',
+      exerciseCount: 20,
+      description: 'Exercise 9.15.',
+      extra: 'A random text',
+    },
   ];
 
   return (
