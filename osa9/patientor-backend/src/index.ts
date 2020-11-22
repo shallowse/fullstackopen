c8fac6066555f6734e74e37d6f66a2f82cda65b6
shallowse <noemail>
2020-11-22 10:59:16 +0000

    osa9: 9.10

--- osa9/patientor-backend/src/index.ts
+++ osa9/patientor-backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import diagnosesRouter from './routes/diagnoses';
+
 const app = express();
 import cors from 'cors';
 
@@ -12,6 +14,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
