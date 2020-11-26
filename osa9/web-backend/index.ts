85f614bb88a56934e97a47c9a46488be0e041bd8
shallowse <noemail>
2020-11-26 18:36:11 +0000

    osa9: checks

--- osa9/web-backend/index.ts
+++ osa9/web-backend/index.ts
@@ -18,7 +18,12 @@ app.get('/bmi', (req, res) => {
   }
 
   if (req.query.height === undefined || req.query.weight === undefined) {
-    res.status(400).json({ error: 'parameters missing' });
+    res.status(400).json({ error: 'parameter(s) missing' });
+    return;
+  }
+
+  if (req.query.height === '' || req.query.weight === '') {
+    res.status(400).json({ error: 'parameter(s) contained nothing' });
     return;
   }
 
@@ -30,15 +35,20 @@ app.get('/bmi', (req, res) => {
     return;
   }
 
-  const bmi: string = calculateBmi(height, weight);
-  console.log(height, weight, bmi);
+  try {
+    const bmi = calculateBmi(height, weight);
+    console.log(height, weight, bmi);
 
-  const retObj = {
-    weight,
-    height,
-    bmi,
-  };
-  res.json(retObj);
+    const retObj = {
+      weight,
+      height,
+      bmi,
+    };
+    res.json(retObj);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
 });
 
 /*
@@ -63,8 +73,8 @@ app.post('/exercises', (req, res) => {
   const tmp: Array<string> = req.body.daily_exercises;
   const dailyHours: Array<number> = tmp.map(x => Number(x));
   for (let i = 0; i < dailyHours.length; i++) {
-    if (isNaN(dailyHours[i])) {
-      res.status(400).json({ error: 'malformatted parameters - array value not a number' });
+    if (isNaN(dailyHours[i]) || dailyHours[i] < 0) {
+      res.status(400).json({ error: 'malformatted parameters - array value not a number or negative' });
       return;
     }
   }
